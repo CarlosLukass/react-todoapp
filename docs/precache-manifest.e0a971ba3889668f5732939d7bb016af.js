f3d88c58b5a9c81cf8f78fe80c5d9cab15630b3b
Carlos Lucas <carlos.lukassf@gmail.com>
2020-09-02 18:05:03 -0500

    rename build folder to 'docs'

---
+++ docs/precache-manifest.e0a971ba3889668f5732939d7bb016af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d845b380e8bdca6a910289572a19d2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec0d98d8a9f7286fd91",
+    "url": "/static/css/main.381ccee0.chunk.css"
+  },
+  {
+    "revision": "f263ed5d273d3a256231",
+    "url": "/static/js/2.4b19d793.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/static/js/2.4b19d793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec0d98d8a9f7286fd91",
+    "url": "/static/js/main.c09ec67f.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  }
+]);
